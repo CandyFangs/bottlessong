c259c3c38f414bbcd8f75ad2e7549ef18f844599
Paulina Osiak <paulina.osiak@gmail.com>
2018-07-07 10:31:53 +0200

    Third test passes

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -4,11 +4,11 @@ class Bottles
     if n < 100 && n > 2
      "#{n} bottles of beer on the wall, #{n} bottles of beer.
       Take one down and pass it around, #{n-1} bottles of beer on the wall!"
-    # elsif n == 89
-    #   '89 bottles of beer on the wall, 89 bottles of beer.
-    #   Take one down and pass it around, 88 bottles of beer on the wall!'
-    # end
+    elsif n == 2
+      '2 bottles of beer on the wall, 2 bottles of beer.
+      Take one down and pass it around, 1 bottle of beer on the wall!'
     end
+
   end
 
 end
