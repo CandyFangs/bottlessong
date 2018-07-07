f8b4c409eb86146b15da4ecbc02faadc2e606d98
Paulina Osiak <paulina.osiak@gmail.com>
2018-07-07 10:26:01 +0200

    Second test passes

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,13 @@
 class Bottles
 
   def verse(n)
+    if n == 99
      '99 bottles of beer on the wall, 99 bottles of beer.
       Take one down and pass it around, 98 bottles of beer on the wall!'
+    elsif n == 89
+      '89 bottles of beer on the wall, 89 bottles of beer.
+      Take one down and pass it around, 88 bottles of beer on the wall!'
+    end
   end
 
 end
