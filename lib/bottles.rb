f90976c6bdba69d4447ff2256fdf63ba2c4da6e1
Paulina Osiak <paulina.osiak@gmail.com>
2018-07-07 10:36:14 +0200

    Fifth test passes

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -10,7 +10,9 @@ class Bottles
     elsif n == 1
       '1 bottle of beer on the wall, 1 bottle of beer.
       Take it down and pass it around, no more bottles of beer on the wall.'
-      
+    else
+      'No more bottles of beer on the wall, no more bottles of beer.
+       Go to the store and buy some more, 99 bottles of beer on the wall.'  
     end
 
   end
