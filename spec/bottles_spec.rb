a06a9d0826a4d1f89fc54307472e572aa9cfd370
Paulina Osiak <paulina.osiak@gmail.com>
2018-07-07 10:33:27 +0200

    Fourth test

--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -18,5 +18,10 @@ RSpec.describe Bottles do
       expect(subject.verse(2)).to eq '2 bottles of beer on the wall, 2 bottles of beer.
       Take one down and pass it around, 1 bottle of beer on the wall!'
     end
+
+    it 'returns propoer description for 1 bottle' do
+      expect(subject.verse(1)).to eq '1 bottle of beer on the wall, 1 bottle of beer.
+      Take it down and pass it around, no more bottles of beer on the wall.'
+    end
   end
 end
