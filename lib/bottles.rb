07a00ba962042a9c299bb84dd8c3e5fc7917d90f
Paulina Osiak <paulina.osiak@gmail.com>
2018-07-07 10:34:12 +0200

    Fourth test passes

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -7,6 +7,10 @@ class Bottles
     elsif n == 2
       '2 bottles of beer on the wall, 2 bottles of beer.
       Take one down and pass it around, 1 bottle of beer on the wall!'
+    elsif n == 1
+      '1 bottle of beer on the wall, 1 bottle of beer.
+      Take it down and pass it around, no more bottles of beer on the wall.'
+      
     end
 
   end
