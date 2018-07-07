077268e55b8d2983622b37c5bbf5377df6d6b845
Paulina Osiak <paulina.osiak@gmail.com>
2018-07-07 11:20:18 +0200

    first refactor

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,20 +1,21 @@
 class Bottles
 
   def verse(n)
-    if n < 100 && n > 2
+
+    case n
+    when 3..99
      "#{n} bottles of beer on the wall, #{n} bottles of beer.
       Take one down and pass it around, #{n-1} bottles of beer on the wall!"
-    elsif n == 2
+    when 2
       '2 bottles of beer on the wall, 2 bottles of beer.
       Take one down and pass it around, 1 bottle of beer on the wall!'
-    elsif n == 1
+    when 1
       '1 bottle of beer on the wall, 1 bottle of beer.
       Take it down and pass it around, no more bottles of beer on the wall.'
     else
       'No more bottles of beer on the wall, no more bottles of beer.
        Go to the store and buy some more, 99 bottles of beer on the wall.'  
     end
-
   end
 
 end
--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -4,29 +4,29 @@ require_relative '../lib/bottles'
 RSpec.describe Bottles do
   describe "#verse" do
 
-    it 'returns propoer description for 99 bottles' do
+    it 'returns proper description for 99 bottles' do
       expect(subject.verse(99)).to eq '99 bottles of beer on the wall, 99 bottles of beer.
       Take one down and pass it around, 98 bottles of beer on the wall!'
     end
 
-    it 'returns propoer description for 89 bottles' do
+    it 'returns proper description for 89 bottles' do
       expect(subject.verse(89)).to eq '89 bottles of beer on the wall, 89 bottles of beer.
       Take one down and pass it around, 88 bottles of beer on the wall!'
     end
 
-    it 'returns propoer description for 2 bottles' do
+    it 'returns proper description for 2 bottles' do
       expect(subject.verse(2)).to eq '2 bottles of beer on the wall, 2 bottles of beer.
       Take one down and pass it around, 1 bottle of beer on the wall!'
     end
 
-    it 'returns propoer description for 1 bottle' do
+    it 'returns proper description for 1 bottle' do
       expect(subject.verse(1)).to eq '1 bottle of beer on the wall, 1 bottle of beer.
       Take it down and pass it around, no more bottles of beer on the wall.'
     end
 
-    it 'returns propoer description for 0 bottles' do
+    it 'returns proper description for 0 bottles' do
       expect(subject.verse(0)).to eq 'No more bottles of beer on the wall, no more bottles of beer.
        Go to the store and buy some more, 99 bottles of beer on the wall.'
     end
-  end
+end
 end
