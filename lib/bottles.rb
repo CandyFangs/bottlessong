8ba2949f9256d9dc3e879f050a1b0412de29405a
Paulina Osiak <paulina.osiak@gmail.com>
2018-07-07 10:22:41 +0200

    First test passed

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,3 +1,8 @@
 class Bottles
 
+  def verse(n)
+     '99 bottles of beer on the wall, 99 bottles of beer.
+      Take one down and pass it around, 98 bottles of beer on the wall!'
+  end
+
 end
--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -3,8 +3,10 @@ require_relative '../lib/bottles'
 
 RSpec.describe Bottles do
   describe "#verse" do
-      it 'returns propoer description for 99 bottles' do
-      expect(subject.verse).to eq '99 bottles of beer on the wall, 99 bottles of beer.
+
+    it 'returns propoer description for 99 bottles' do
+      expect(subject.verse(99)).to eq '99 bottles of beer on the wall, 99 bottles of beer.
       Take one down and pass it around, 98 bottles of beer on the wall!'
+    end
   end
 end
